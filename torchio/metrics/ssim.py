0b8545688c43dca06833ed6f2117bc0630090957
valabregue <romain.valabregue@upmc.fr>
2020-11-30 15:06:44 +0100

    add kernel_size argument for uniform kernel, change default k1 k2 k3, to bigger values, add other patch based metric normalized l2

--- torchio/metrics/ssim.py
+++ torchio/metrics/ssim.py
@@ -7,8 +7,8 @@ from ..torchio import DATA
 
 class SSIM3D(MapMetric):
 
-    def __init__(self, metric_name="SSIM", k1=.001, k2=.001, k3=.001, L=None, alpha=1, beta=1, gamma=1, kernel="uniform", sigma=3.0,
-                 truncate=4.0, **kwargs):
+    def __init__(self, metric_name="SSIM", k1=.01, k2=.01, k3=.01, L=None, alpha=1, beta=1, gamma=1, kernel="uniform", sigma=3.0,
+                 kernel_size=3, truncate=3, **kwargs):
         super(SSIM3D, self).__init__(metric_name=metric_name, **kwargs)
         self.k1 = k1
         self.k2 = k2
@@ -21,8 +21,10 @@ class SSIM3D(MapMetric):
         if self.kernel == "gaussian":
             self.sigma = sigma
             self.truncate = truncate
+            self.kernel_size = None
         else:
             self.sigma, self.truncate = None, None
+            self.kernel_size = kernel_size
 
     def apply_metric(self, sample1: Subject, sample2: Subject):
         common_keys = self.get_common_intensity_keys(sample1=sample1, sample2=sample2)
@@ -35,8 +37,8 @@ class SSIM3D(MapMetric):
             data2 = sample2[sample_key][DATA]
             computed_metrics = functional_ssim(data1, data2, k1=self.k1, k2=self.k2, k3=self.k3,
                                                     L=self.L, alpha=self.alpha, beta=self.beta, gamma=self.gamma,
-                                                    kernel=self.kernel, sigma=self.sigma, truncate=self.truncate,
-                                                    return_map=True)
+                                                    kernel=self.kernel, sigma=self.sigma, kernel_size=self.kernel_size,
+                                                    truncate=self.truncate, return_map=True)
 
             for m_name, m_map in computed_metrics.items():
                 metric_dict = self._apply_masks_and_averaging(sample=sample2, metric_map=m_map)
@@ -44,12 +46,12 @@ class SSIM3D(MapMetric):
                     if mask_name is "no_mask":
                         computed_metrics_res[sample_key]["{}_{}".format(self.metric_name, m_name)] = masked_metric
                     else:
-                        computed_metrics_res[sample_key]["{}_{}_{}".format(self.metric_name, m_name, mask_name)] = masked_metric
+                        computed_metrics_res[sample_key]["{}_{}".format( m_name, mask_name)] = masked_metric
         return computed_metrics_res
 
 
-def functional_ssim(x, y, k1=.001, k2=.001, k3=.001, L=None, alpha=1, beta=1, gamma=1, kernel="uniform", sigma=3.0,
-                    truncate=4.0, return_map=False):
+def functional_ssim(x, y, k1=.01, k2=.01, k3=.01, L=None, alpha=1, beta=1, gamma=1, kernel="uniform", sigma=3.0,
+                    kernel_size=3, truncate=3, return_map=False, clamp_ssim = False):
     """
     Computes the structural similarity between x and y
     Args:
@@ -84,9 +86,10 @@ def functional_ssim(x, y, k1=.001, k2=.001, k3=.001, L=None, alpha=1, beta=1, ga
         kernel_params = gauss_kernel_3d(sigma=sigma, truncate=truncate)
         kernel_params = kernel_params.reshape(1, 1, *kernel_params.shape).float()
     else:
-        kernel_params = torch.ones((1, 1, 3, 3, 3))
-        kernel_params /= 27
+        kernel_params = torch.ones((1, 1, kernel_size, kernel_size, kernel_size))
+        kernel_params /= kernel_size ** 3
 
+    #print('sim K {} is {}'.format(kernel, kernel_params))
     c1 = (k1 * L) ** 2
     c2 = (k2 * L) ** 2
     c3 = (k3 * L) ** 2
@@ -95,16 +98,21 @@ def functional_ssim(x, y, k1=.001, k2=.001, k3=.001, L=None, alpha=1, beta=1, ga
     x_sub, y_sub = x - mu_x, y - mu_y
     std_x, std_y = spatial_filter_nd(x_sub.pow(2), kernel=kernel_params).sqrt(), \
                    spatial_filter_nd(y_sub.pow(2), kernel=kernel_params).sqrt()
-    std_xy = spatial_filter_nd(torch.mul(x_sub, y_sub), torch.ones_like(kernel_params))
-    std_xy /= (kernel_params.numel() - 1)
+    #std_xy = spatial_filter_nd(torch.mul(x_sub, y_sub), torch.ones_like(kernel_params))
+    #std_xy /= (kernel_params.numel() - 1)
+    std_xy = spatial_filter_nd(torch.mul(x_sub, y_sub),  kernel=kernel_params)
+
 
     luminance = (2 * mu_x * mu_y + c1) / (mu_x ** 2 + mu_y ** 2 + c1)
     contrast = (2 * std_x * std_y + c2) / (std_x ** 2 + std_y ** 2 + c2)
     structure = (std_xy + c3) / ((std_x * std_y) + c3)
+
+    l2_xy = spatial_filter_nd((x_sub - y_sub).pow(2), kernel=kernel_params).sqrt()
+
     #Keep values between 0.0 and 1.0
-    luminance = luminance.clamp(0.0, 1.0)
-    contrast = contrast.clamp(0.0, 1.0)
-    structure = structure.clamp(0.0, 1.0)
+    #luminance = luminance.clamp(0.0, 1.0)
+    #contrast = contrast.clamp(0.0, 1.0)
+    #structure = structure.clamp(0.0, 1.0)
     ssim = luminance ** alpha * contrast ** beta * structure ** gamma
 
     if not return_map:
@@ -113,10 +121,12 @@ def functional_ssim(x, y, k1=.001, k2=.001, k3=.001, L=None, alpha=1, beta=1, ga
         contrast = contrast.mean()
         structure = structure.mean()
         ssim = ssim.mean()
+        l2_xy = l2_xy.mean()
 
     return {
         "luminance": luminance,
         "contrast": contrast,
         "structure": structure,
-        "ssim": ssim
+        "ssim": ssim,
+        "l2ns": l2_xy,
     }
