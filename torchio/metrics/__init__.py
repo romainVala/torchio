8d3732c16022bc7be89e5aac0b4c66126824dff6
valabregue <romain.valabregue@upmc.fr>
2021-02-09 16:30:41 +0100

    add more metrics

--- torchio/metrics/__init__.py
+++ torchio/metrics/__init__.py
@@ -2,6 +2,6 @@ from .base_metric import Metric
 from .map_metric import MapMetric
 from .map_metric_wrapper import MapMetricWrapper
 from .ssim import SSIM3D
-from .peak_signal_to_noise_ratio import PSNR, NRMSE
+from .peak_signal_to_noise_ratio import PSNR, NRMSE, GradRatio, LabelMetric
 from .normalized_cross_correlation import NCC, NCC_conv
 from .visual_information_fidelity import VIF
--- torchio/metrics/peak_signal_to_noise_ratio.py
+++ torchio/metrics/peak_signal_to_noise_ratio.py
@@ -2,6 +2,12 @@ import torch
 from typing import Union, List, Callable
 from .map_metric_wrapper import MapMetricWrapper
 import torch.nn.functional as F
+import numpy as np
+from ..data import Subject
+from ..constants import DATA
+from scipy import ndimage
+
+EPS = np.finfo(float).eps
 
 def _nrmse(input, target, normalization='euclidean'):
     '''
@@ -76,6 +82,142 @@ def _psnr(input, target, normalization='max'):
     psnrs = 20.0 * torch.log10(maximum_value) - 10.0 * torch.log10(mean_square_error)
     return psnrs
 
+def _entropy(x):
+    #simple definition from autofocusing literature
+    x = x.ravel()
+    x = x[abs(x)>1e-6]
+    bmax = bmax = np.square(np.sum(x**2)) #np.sum(x**2) #change le 9 02 2021
+    proba = x / bmax
+    return - np.sum( np.log(x)*x )
+
+def nmi(x, y, **kwargs):
+    """ Mutual information for joint histogram Elina
+    """
+    # Convert bins counts to probability values
+    bins = (256, 256)
+    hist_inter, _, _ = np.histogram2d(x.ravel(), y.ravel(), bins=bins)
+    hist1, _, _ = np.histogram2d(x.ravel(), x.ravel(), bins=bins)
+    hist2, _, _ = np.histogram2d(y.ravel(), y.ravel(), bins=bins)
+
+    return 2 * _mutual_information(hist_inter) / (
+            _mutual_information(hist1) + _mutual_information(hist2))
+
+def _mutual_information(hgram):
+    pxy = hgram / float(np.sum(hgram))
+    px = np.sum(pxy, axis=1)  # marginal for x over y
+    py = np.sum(pxy, axis=0)  # marginal for y over x
+    px_py = px[:, None] * py[None, :]  # Broadcast to multiply marginals
+    # Now we can do the calculation using the pxy, px_py 2D arrays
+    nzs = pxy > 0  # Only non-zero pxy values contribute to the sum
+    return np.sum(pxy[nzs] * np.log(pxy[nzs] / px_py[nzs]))
+
+def mutual_information_2d(x, y, sigma=1, normalized=True):
+    """
+    Computes (normalized) mutual information between two 1D variate from a
+    joint histogram.
+    Parameters
+    ----------
+    x : 1D array
+        first variable
+    y : 1D array
+        second variable
+    sigma: float
+        sigma for Gaussian smoothing of the joint histogram
+    Returns
+    -------
+    nmi: float
+        the computed similariy measure
+    """
+    bins = (256, 256)
+
+    jh = np.histogram2d(x.ravel(), y.ravel(), bins=bins)[0]
+
+    # smooth the jh with a gaussian filter of given sigma
+    ndimage.gaussian_filter(jh, sigma=sigma, mode='constant',
+                                 output=jh)
+
+    # compute marginal histograms
+    jh = jh + EPS
+    sh = np.sum(jh)
+    jh = jh / sh
+    s1 = np.sum(jh, axis=0).reshape((-1, jh.shape[0]))
+    s2 = np.sum(jh, axis=1).reshape((jh.shape[1], -1))
+
+    # Normalised Mutual Information of:
+    # Studholme,  jhill & jhawkes (1998).
+    # "A normalized entropy measure of 3-D medical image alignment".
+    # in Proc. Medical Imaging 1998, vol. 3338, San Diego, CA, pp. 132-143.
+    if normalized:
+        mi = ((np.sum(s1 * np.log(s1)) + np.sum(s2 * np.log(s2)))
+                / np.sum(jh * np.log(jh))) - 1
+    else:
+        mi = ( np.sum(jh * np.log(jh)) - np.sum(s1 * np.log(s1))
+               - np.sum(s2 * np.log(s2)))
+
+    return 2*mi
+
+
+def _grad_ratio(input,target, do_scat=False, do_nmi=True, do_entropy=True):
+    #print(f' i shape {input.shape}')
+     #not sure how to handel batch size (first dim) TODO
+    input = input[0]
+    target = target[0]
+
+    grad_i = np.gradient(input)
+    grad_t = np.gradient(target)
+
+    grad_sum_i=np.zeros_like(grad_i[0])
+    for gg in grad_i :
+        grad_sum_i += np.abs(gg)
+
+    grad_sum_t=np.zeros_like(grad_t[0])
+    for gg in grad_t :
+        grad_sum_t += np.abs(gg)
+
+    #grad_sum_i = np.sum([np.sum(np.abs(gg)) for gg in grad_i])
+    #grad_sum_t = np.sum([np.sum(np.abs(gg)) for gg in grad_t])
+    grad_mean_i = np.mean(grad_sum_i)
+    grad_mean_t = np.mean(grad_sum_t)
+
+    #mean only on edge ... (like AES metric)
+    grad_mean_edge_i = np.mean( grad_sum_i[grad_sum_i > 0.01])
+    grad_mean_edge_t = np.mean( grad_sum_t[grad_sum_t > 0.01])
+
+    res_dict=dict()
+    res_dict['ratio'] = grad_mean_i/grad_mean_t
+    res_dict['ratio_bin'] = grad_mean_edge_i/grad_mean_edge_t
+
+    if do_scat:
+        from kymatio import HarmonicScattering3D
+        import time
+        data =  dd= torch.stack([input,target])
+        data_shape = data.shape[1:]
+        print(data_shape)
+        J = 2;        L = 2;        integral_powers = [1., 2.];        sigma_0 = 1
+        scattering = HarmonicScattering3D(J, shape=data_shape, L=L, sigma_0=sigma_0)
+        scattering.method = 'integral'
+        scattering.integral_powers = integral_powers
+        s=time.time()
+        res = scattering(data)
+        s=time.time()-s
+        print(f'scat in {s}')
+        res_dict['scat'] = torch.norm(res[0]-res[1])
+    if do_nmi:
+        res_dict['nMI1'] = nmi(input.numpy(), target.numpy())
+        res_dict['nMI2'] = mutual_information_2d(input.numpy(), target.numpy())
+    if do_entropy:
+        res_dict['Eorig']  = _entropy(input.numpy())
+        res_dict['Emot']   = _entropy(target.numpy())
+        # res_dict['Emot2'] = nmi(target.numpy(), target.numpy()) #faudrait une version non normalise ...
+        res_dict['Eratio'] = res_dict['Eorig'] / res_dict['Emot']
+        entro_grad1 = np.sum([ _entropy(np.abs(gg)) for gg in grad_i])
+        entro_grad2 = np.sum([ _entropy(np.abs(gg)) for gg in grad_t])
+        res_dict['EGratio'] = entro_grad1 / entro_grad2
+
+    return res_dict
+
+def _identity():
+    pass
 
 class PSNR(MapMetricWrapper):
 
@@ -92,3 +234,61 @@ class NRMSE(MapMetricWrapper):
         super(NRMSE, self).__init__(metric_name=metric_name, metric_func=metric_func, select_key=select_key,
                                    scale_metric=scale_metric, average_method=average_method,
                                    save_in_subject_keys=save_in_subject_keys, metric_kwargs=metric_kwargs, **kwargs)
+class GradRatio(MapMetricWrapper):
+
+    def __init__(self, metric_name: str = "Gratio", metric_func: Callable = _grad_ratio, select_key: Union[List, str] = None, scale_metric: float = 1,
+                 average_method: str = None, save_in_subject_keys: bool = False, metric_kwargs: dict = None, **kwargs):
+        super(GradRatio, self).__init__(metric_name=metric_name, metric_func=metric_func, select_key=select_key,
+                                   scale_metric=scale_metric, average_method=average_method,
+                                   save_in_subject_keys=save_in_subject_keys, metric_kwargs=metric_kwargs, **kwargs)
+
+
+    def apply_metric(self, sample1: Subject, sample2: Subject):
+        common_keys = self.get_common_intensity_keys(sample1=sample1, sample2=sample2)
+        computed_metrics_res = dict()
+        for sample_key in common_keys:
+            if sample_key in self.mask_keys:
+                continue
+            computed_metrics_res[sample_key] = dict()
+            data1 = sample1[sample_key][DATA]
+            data2 = sample2[sample_key][DATA]
+            computed_metrics = self.metric_func(data1, data2)
+
+            for m_name, m_map in computed_metrics.items():
+                #metric_dict = self._apply_masks_and_averaging(sample=sample2, metric_map=m_map)
+                #for mask_name, masked_metric in metric_dict.items():
+                #    if mask_name == "no_mask":
+                computed_metrics_res[sample_key]["{}_{}".format(self.metric_name, m_name)] = m_map#masked_metric
+                #    else:
+                #        computed_metrics_res[sample_key]["{}_{}".format( m_name, mask_name)] = masked_metric
+        return computed_metrics_res
+
+
+class LabelMetric(MapMetricWrapper):
+
+    def __init__(self, metric_name: str = "lab", metric_func: Callable = _identity, select_key: Union[List, str] = None, scale_metric: float = 1,
+                 average_method: str = None, save_in_subject_keys: bool = False, metric_kwargs: dict = None, **kwargs):
+        super(LabelMetric, self).__init__(metric_name=metric_name, metric_func=metric_func, select_key=select_key,
+                                   scale_metric=scale_metric, average_method=average_method,
+                                   save_in_subject_keys=save_in_subject_keys, metric_kwargs=metric_kwargs, **kwargs)
+
+    def apply_metric(self, sample1: Subject, sample2: Subject):
+        common_keys = self.get_common_intensity_keys(sample1=sample1, sample2=sample2)
+        computed_metrics_res = dict()
+        for sample_key in common_keys:
+            if sample_key in self.mask_keys:
+                continue
+            computed_metrics_res[sample_key] = dict()
+            data1 = sample1[sample_key][DATA][0] #arg todo if batch size >1 ... ?
+            data2 = sample2[sample_key][DATA][0]
+            labels = sample1['label'][DATA]
+            ind = labels>0.5 #binarize
+
+            mean_ratio = [data1[li].mean()/data2[li].mean() for li in ind]
+            std_ratio = [data1[li].std()/data2[li].std() for li in ind]
+
+            for i, (one_mean, one_std) in enumerate(zip(mean_ratio,std_ratio)):
+                computed_metrics_res[sample_key]["{}_mean{}".format(self.metric_name, i)] = one_mean
+                computed_metrics_res[sample_key]["{}_std{}".format(self.metric_name, i)] = one_std
+
+        return computed_metrics_res
